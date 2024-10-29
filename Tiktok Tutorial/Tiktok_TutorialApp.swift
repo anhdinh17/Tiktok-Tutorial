4fce0c29c8ccf6f3353d1d77ed215afe8a69308d
Anh Dinh <anhdinh17@gmail.com>
2024-10-28 22:15:30 -0500

    Initial Commit

---
+++ Tiktok Tutorial/Tiktok_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiktok_TutorialApp.swift
+//  Tiktok Tutorial
+//
+//  Created by Anh Dinh on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Tiktok_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
