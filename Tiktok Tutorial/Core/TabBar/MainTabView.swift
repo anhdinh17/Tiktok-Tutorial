8bf4634bb400b1d026463523ec53eac696523000
Anh Dinh <anhdinh17@gmail.com>
2024-10-30 05:18:22 -0500

    MaintabView

---
+++ Tiktok Tutorial/Core/TabBar/MainTabView.swift
@@ -0,0 +1,74 @@
+//
+//  MainTabView.swift
+//  Tiktok Tutorial
+//
+//  Created by Anh Dinh on 10/29/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State var selectedTab = 0
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Text("Feed")
+                .tabItem {
+                    VStack {
+                        Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                            // it's syntax for selecting a tab and make it fill
+                            .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                        Text("Home")
+                    }
+                }
+            // .onAppear and .tag are to make the image fill when selecting a tab and
+            // not fill when we select other tab
+            // combining with logic in VStack
+                .onAppear{ selectedTab = 0}
+                .tag(0)
+            
+            Text("Friends")
+                .tabItem {
+                    VStack {
+                        Image(systemName: selectedTab == 1 ? "person.2.fill" : "person.2")
+                            .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                        Text("Friends")
+                    }
+                }
+                .onAppear{ selectedTab = 1}
+                .tag(1)
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    VStack {
+                        Image(systemName: selectedTab == 3 ? "heart.fill" : "heart")
+                            .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                        Text("Inbox")
+                    }
+                }
+                .onAppear{ selectedTab = 3}
+                .tag(3)
+            
+            Text("Profile")
+                .tabItem {
+                    VStack {
+                        Image(systemName: selectedTab == 4 ? "person.fill" : "person")
+                            .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
+                        Text("Profile")
+                    }
+                }
+                .onAppear{ selectedTab = 4}
+                .tag(4)
+        }
+        .tint(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
